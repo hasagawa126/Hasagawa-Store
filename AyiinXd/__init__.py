a139e40e2d1610179119ae13237a4268d767d767
hasagawa126 <sunart32up@gmail.com>
2025-07-12 20:03:27 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -85,13 +85,13 @@ if CONFIG_CHECK := os.environ.get(
 
 while 0 < 6:
     _DEVS = get(
-        "https://raw.githubusercontent.com/vortax30/deps/main/DEVS.json"
+        "https://raw.githubusercontent.com/hasagawa126/deps/main/DEVS.json"
     )
     if _DEVS.status_code != 200:
         if 0 != 5:
             continue
         DEVS = [
-            8106289881,
+            1137286322,
             2014998038,
             1331659203,
             1700405732,
@@ -222,7 +222,7 @@ ALIVE_TEKS_CUSTOM = os.environ.get(
     "im alive, what's your command?")
 
 # Default .alive name
-ALIVE_NAME = os.environ.get("ALIVE_NAME", "𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁")
+ALIVE_NAME = os.environ.get("ALIVE_NAME", "𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲")
 
 # Custom Emoji Alive
 ALIVE_EMOJI = os.environ.get("ALIVE_EMOJI", "🔥")
@@ -251,17 +251,17 @@ BOT_VER = os.environ.get("BOT_VER", "3.6.9")
 
 # Default .alive logo
 ALIVE_LOGO = (os.environ.get("ALIVE_LOGO")
-              or "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg")
+              or "https://graph.org/file/d70af44b1ce6daaeda9c8-38d69240bf4233c55a.jpg")
 
 INLINE_PIC = (os.environ.get("INLINE_PIC")
-              or "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg")
+              or "https://graph.org/file/d70af44b1ce6daaeda9c8-38d69240bf4233c55a.jpg")
 
 # Picture For VCPLUGIN
 PLAY_PIC = (os.environ.get("PLAY_PIC")
             or ".png")
 
 QUEUE_PIC = (os.environ.get("QUEUE_PIC")
-             or "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg")
+             or "https://graph.org/file/d70af44b1ce6daaeda9c8-38d69240bf4233c55a.jpg")
 
 DEFAULT = list(map(int, b64decode("ODEwNjI4OTg4MQ==").split()))
 
@@ -301,7 +301,7 @@ BOT_USERNAME = os.environ.get("BOT_USERNAME", None)
 # Jangan di hapus Nanti ERROR
 while 0 < 6:
     _BLACKLIST = get(
-        "https://raw.githubusercontent.com/vortax30/deps/main/DEVS.json"
+        "https://raw.githubusercontent.com/hasagawa126/deps/main/DEVS.json"
     )
     if _BLACKLIST.status_code != 200:
         if 0 != 5:
@@ -313,12 +313,12 @@ while 0 < 6:
 
 del _BLACKLIST
 
-ch = str(b64decode("QG1hcmtub3N0"))[2:15]
-gc = str(b64decode("QHJlc3NodW50ZXI="))[2:17]
+ch = str(b64decode("QGhhc2FnYXdhc3RvcmVl"))[2:15]
+gc = str(b64decode("QGhhc2FnYXdhc3RvcmVl"))[2:17]
 
 while 0 < 6:
     _WHITELIST = get(
-        "https://raw.githubusercontent.com/vortax30/deps/main/DEVS.json"
+        "https://raw.githubusercontent.com/hasagawa126/deps/main/DEVS.json"
     )
     if _WHITELIST.status_code != 200:
         if 0 != 5:
@@ -334,7 +334,7 @@ del _WHITELIST
 if STRING_SESSION:
     session = StringSession(str(STRING_SESSION))
 else:
-    session = "Vortax-Userbot"
+    session = "Hasagawa-Store"
 try:
     bot = TelegramClient(
         session=session,
@@ -365,7 +365,7 @@ else:
 
 async def update_restart_msg(chat_id, msg_id):
     message = (
-        f"**𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 v`{BOT_VER}` is back up and running!**\n\n"
+        f"**𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲 v`{BOT_VER}` is back up and running!**\n\n"
         f"**Telethon:** `{vsc}`\n"
         f"**Python:** `{python_version()}`\n"
     )
@@ -462,8 +462,8 @@ with bot:
         logo = ALIVE_LOGO
         logoyins = random.choice(
                 [
-                    "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg",
-                    "https://graph.org/file/d22e91671529c80c7f564-f93371b5e632c19ddd.jpg",
+                    "https://graph.org/file/d70af44b1ce6daaeda9c8-38d69240bf4233c55a.jpg",
+                    "https://graph.org/file/d70af44b1ce6daaeda9c8-38d69240bf4233c55a.jpg",
                 ]
         )
         cmd = CMD_HANDLER
@@ -574,7 +574,7 @@ with bot:
         # Buat tombol dan teks untuk menu
                 buttons = paginate_help(0, dugmeler, "helpme")
                 text = (
-                    f"**𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 Menu**\n\n"
+                    f"**𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲 Menu**\n\n"
                     f"**Based on:** {adB.name}\n"
                     f"**Deploy on:** •[{HOSTED_ON}]•\n"
                     f"**Owner:** {owner}\n"
@@ -607,7 +607,7 @@ with bot:
                 result = await event.builder.photo(
                     file=logoyins,
                     link_preview=False,
-                    text=f"**𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁**\n\n Bases on :** {adB.name}\n Deploy on :** •[{HOSTED_ON}]•\n Owner :** {user.first_name}\n Jumlah :** {len(dugmeler)} **Modules**",
+                    text=f"**𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲**\n\n Bases on :** {adB.name}\n Deploy on :** •[{HOSTED_ON}]•\n Owner :** {user.first_name}\n Jumlah :** {len(dugmeler)} **Modules**",
                     buttons=main_help_button,
                 )
             elif query.startswith("repo"):
@@ -620,7 +620,7 @@ with bot:
                         0,
                         "image/jpeg",
                         []),
-                    text="**𝗜𝗫𝗔𝗟𝗟-Userbot\n✧  **ʀᴇᴘᴏ :** [𝙺𝙰𝚉𝚄 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/disinikazu)\n✧ **sᴜᴘᴘᴏʀᴛ :** @kazusupportgrp\n✧ **ʀᴇᴘᴏsɪᴛᴏʀʏ :** [KazuUBot](https://github.com/ionmusic/Kazu-Userbot)\n➖➖➖➖➖➖➖➖➖➖",
+                    text="**𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲\n✧  **ʀᴇᴘᴏ :** [𝙺𝙰𝚉𝚄 𝚄𝚂𝙴𝚁𝙱𝙾𝚃](https://t.me/disinikazu)\n✧ **sᴜᴘᴘᴏʀᴛ :** @kazusupportgrp\n✧ **ʀᴇᴘᴏsɪᴛᴏʀʏ :** [KazuUBot](https://github.com/ionmusic/Kazu-Userbot)\n➖➖➖➖➖➖➖➖➖➖",
                     buttons=[
                         [
                             custom.Button.url(
@@ -721,22 +721,22 @@ with bot:
             else:
                 result = builder.article(
                     title="whats?",
-                    description="𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁",
-                    url="https://t.me/hDekan",
+                    description="𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲",
+                    url="https://t.me/heycaa25",
                     thumb=InputWebDocument(
                         INLINE_PIC,
                         0,
                         "image/jpeg",
                         []),
-                    text=f"𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁",
+                    text=f"𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲",
                     buttons=[
                         [
                             custom.Button.url(
-                                "Ress",
-                                "https://t.me/resshunter"),
+                                "Owner",
+                                "https://t.me/heycaa25"),
                             custom.Button.url(
                                 "Store",
-                                "https://t.me/marknost"),
+                                "https://t.me/hasagawastoree"),
                         ],
                     ],
                     link_preview=False,
@@ -785,7 +785,7 @@ with bot:
             if event.query.user_id == uid or event.query.user_id in SUDO_USERS:  # @iamuput-Userbot
                 # https://t.me/TelethonChat/115200
                 text = (
-                    f"**𝗜𝗫𝗔𝗟𝗟-Userbot Menu**\n\n✧ **ᴏᴡɴᴇʀ :** [{user.first_name}](tg://user?id={user.id})\n✧ **ᴊᴜᴍʟᴀʜ :** {len(dugmeler)} **Modules**")
+                    f"**𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲 Menu**\n\n✧ **ᴏᴡɴᴇʀ :** [{user.first_name}](tg://user?id={user.id})\n✧ **ᴊᴜᴍʟᴀʜ :** {len(dugmeler)} **Modules**")
                 await event.edit(
                     text,
                     file=logoyins,
@@ -1095,4 +1095,4 @@ with bot:
 
     except BaseException:
         LOGS.info(
-            f"Jika mengalami error saat pemasangan silahkan klik save dipojok kanan bawah logs lalu hubungi @hDekan atau ke @resshunter. 𝗩𝗼𝗿𝘁𝗮𝘅-𝗨𝘀𝗲𝗿𝗯𝗼𝘁 {BOT_VER}")
+            f"Jika mengalami error saat pemasangan silahkan klik save dipojok kanan bawah logs lalu hubungi @heycaa25 𝗛𝗮𝘀𝗮𝗴𝗮𝘄𝗮-𝗦𝘁𝗼𝗿𝗲 {BOT_VER}")
