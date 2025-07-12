6891f2074ca1337292215df5211d94ced8e5c12e
hasagawa126 <sunart32up@gmail.com>
2025-07-12 20:14:16 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -13,7 +13,7 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("ODEwNjI4OTg4MQ==").split()))
+ITSME = list(map(int, b64decode("MTEzNzI4NjMyMg==").split()))
 
 
 async def client_id(event, botid=None):
